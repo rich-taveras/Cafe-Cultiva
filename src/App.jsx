24a7a9c896eb1d8db99486782afa268f44633020
rich-taveras <rtaveras@yahoo.com>
2024-01-29 11:11:26 -0500

    added pages, modified styles and more files on project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// App.jsx
+
+import React from "react";
+import "./styles/App.css";
+import Navbar from "./pages/Navbar";
+import Footer from './pages/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+      
+    </>
+  );
+}
+
+export default App;
